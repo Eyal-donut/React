6b2c02aa961e21fd7a7c126821dba60fa8f50afd
Eyal-donut <LilaEyal@gmail.com>
2023-02-15 14:14:15 +0100

    8.1_calasses

--- template_app/src/Name.js
+++
@@ -1,4 +0,0 @@
-const Name = ({name}) => {
-    return <li>{name}</li>
-}
-export default Name
---
+++ template_app/src/components/Box3.js
@@ -0,0 +1,13 @@
+import Box4 from "./Box4"
+import Wrapper from "./Helpers/Wrapper"
+
+const Box3 = () => {
+    return (
+      <div className="box-three">
+          <Box4/>
+          <Box4/>
+      </div>
+    )
+  }
+  
+  export default Box3
