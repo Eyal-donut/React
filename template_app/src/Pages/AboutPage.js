b4e5b3b59d7fb697a2230b7f4d662af98e9afa59
Eyal-donut <LilaEyal@gmail.com>
2023-02-27 15:20:51 +0100

    20.1

--- template_app/src/Pages/AboutPage.js
+++
@@ -1,8 +0,0 @@
-import { useGlobalContext } from "../context/context"
-
-const AboutPage = () => {
-    const {mode} = useGlobalContext()
-    return <h1 className={mode==='day'? 'day':'night'}>About Page</h1>
-}
-
-export default AboutPage
--- template_app/src/Pages/HomePage.js
+++
@@ -1,8 +0,0 @@
-import { useGlobalContext } from "../context/context"
-
-const HomePage = () => {
-    const {mode} = useGlobalContext()
-    return <h1 className={mode==='day'? 'day':'night'}>Home Page</h1>
-}
-
-export default HomePage
