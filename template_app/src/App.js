d38b11c50673b15044ce1d6e7ea1ce81eb43859b
Eyal-donut <LilaEyal@gmail.com>
2023-02-27 16:35:47 +0100

    20.3

--- template_app/src/App.js
+++ template_app/src/App.js
@@ -4,80 +4,61 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      css: true,
-      javascript: true,
-      learnReact: false,
-      learnMongoDB: false,
-      learnNodeJS: false,
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      
     };
   }
-  handleClick(e) {
-    console.log(e.target.parentElement.className);
+  handleInputChange(e) {
+    // console.log(e.target.parentElement.className);
     switch (e.target.parentElement.className) {
-      case "css":
-        this.setState((prevState) => {
-          return { css: !prevState.css };
-        });
+      case "seconds":
+        this.setState({
+          seconds: e.target.value,
+          minutes: (e.target.value)/60,
+          hours: ((e.target.value)/60)/60,
+        })
         break;
-      case "javaScript":
-        this.setState((prevState) => {
-          return { javascript: !prevState.javascript };
-        });
-        break;
-      case "learn-react":
-        this.setState((prevState) => {
-          return { learnReact: !prevState.learnReact };
-        });
-        break;
-      case "learn-mongo-db":
-        this.setState((prevState) => {
-          return { learnMongoDB: !prevState.learnMongoDB };
-        });
+      case "minutes":
+        this.setState({
+          seconds: (e.target.value)*60,
+          minutes: e.target.value,
+          hours: (e.target.value)/60,
+        })
         break;
       default:
-        this.setState((prevState) => {
-          return { learnNodeJS: !prevState.learnNodeJS };
-        });
+        this.setState({
+          seconds: ((e.target.value)*60)*60,
+          minutes: (e.target.value)*60,
+          hours: e.target.value,
+        })
     }
   }
   render() {
     return (
       <>
         <ul>
-          <li className="css">
-            <p>CSS</p>
-            <button
-              onClick={this.handleClick.bind(this)}
-              className={this.state.css ? "true" : "false"}
-            ></button>
+          <li className="hours">
+            <p>Hours</p>
+            <input
+              onChange={this.handleInputChange.bind(this)}
+              placeholder= {this.state.hours}
+            ></input>
           </li>
-          <li className="javaScript">
-            <p>JavaScript</p>
-            <button
-              onClick={this.handleClick.bind(this)}
-              className={this.state.javascript ? "true" : "false"}
-            ></button>
+          <li className="minutes">
+            <p>Minutes</p>
+            <input
+              onChange={this.handleInputChange.bind(this)}
+              placeholder= {this.state.minutes}
+            ></input>
           </li>
-          <li className="learn-react">
-            <p>Learn React</p>
-            <button
-              onClick={this.handleClick.bind(this)}
-              className={this.state.learnReact ? "true" : "false"}
-            ></button>
-          </li>
-          <li className="learn-mongo-db">
-            <p>Learn mongo DB</p>
-            <button
-              onClick={this.handleClick.bind(this)}
-              className={this.state.learnMongoDB ? "true" : "false"}
-            ></button>
-          </li>
-          <li className="learn-node-js">
-            <p>Learn Node JS</p>
-            <button
-              onClick={this.handleClick.bind(this)}
-              className={this.state.learnNodeJS ? "true" : "false"}
-            ></button>
+          <li className="seconds">
+            <p>Seconds</p>
+            <input
+              onChange={this.handleInputChange.bind(this)}
+              placeholder= {this.state.seconds}
+            ></input>
           </li>
         </ul>
       </>
